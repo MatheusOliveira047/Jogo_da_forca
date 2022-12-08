6a635a645062f9a67254aef3e77e0ba950afdef3
MatheusOliveira047 <ludsonmatheus@gmail.com>
2022-12-07 23:36:29 -0300

    configurações inicias do projeto

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordList = {
+  carro: ['Motor', 'Porta', 'Capô', 'Pneu', 'Antena', 'Parachoque', 'Retrovisor', 'seta'],
+  fruta: ['Açai','Banan','Pêra', 'Mamão', 'Laranja', 'Uva','Kwui','Manga'],
+  corpo: ['Braça','Perna','Cérebro', 'Pescoço', 'Olhos', 'Orelha', 'Nariz', 'Boca'],
+  computador:['Mouse','Teclado', 'Monitor', 'Gabinete','CPU','Cooler'],
+  programação:['Python', 'Javacript', 'FrontEnd','BackEnd','React','Node','Java','VSCODE'],
+  alimento:['Arroz','Feijão','Carne','Leite','Ovo','Frango','Batata']
+}
